78f3606a4ed765845c5ddb10f7947ece866abae0
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-14 21:07:38 -0500

    Creación de AppFooter.vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<template>
+    <footer>
+        <p>&#169 H2O MÉXICO</p>
+        <p>launchx00637@innovaccion.mx</p>
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        background-color: #0096C7;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+    }
+    
+    p {
+        color: white;
+        font-weight: 500;
+        font-size: small;
+        font-style: italic;
+    }
+</style>
