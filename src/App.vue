a2f659f7486f761b5ee6eefdf54c31d587b5d0a7
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-14 21:09:21 -0500

    Agregando fuente Montserrat y AppFooter en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
+import AppFooter from "@/components/AppFooter.vue";
 </script>
 
 <template>
-<NavBar/>
+  <div class="App">
+    <NavBar/>
+    <AppFooter/>
+  </div>
 </template>
 
 <style>
-
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;1,500;1,600&display=swap');
+  .App {
+    font-family: 'Montserrat';
+  }
 </style>
