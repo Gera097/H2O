b474de64e08dcc67aaf8e504427b5cce1978c47c
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-15 03:38:29 -0500

    Creada AnimalCard.vue

---
+++ src/components/AnimalCard.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="card">
+    <img src="@/assets/Ajolote.png" alt="Ajolote">
+    <button>{{animal_name}}</button>
+</div>
+</template>
+
+<style scoped>
+</style>
+<script setup>
+</script>
---
+++ src/views/AnimalsView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import AnimalCard from "@/components/AnimalCard.vue";
+</script>
+<template>
+<AnimalCard/>
+</template>
+<style>
+</style>
