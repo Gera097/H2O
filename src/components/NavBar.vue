5c0c01b27cf96f9617fd95c0e210aae6cbe7f705
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-14 20:05:42 -0500

    Creada barra de navegación simple

---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <nav class="wrapper">
+            <h1>H2O MÉXICO</h1>
+            <div>
+                <RouterLink class="link" to="/">Home</RouterLink>
+                <RouterLink class="link" to="/about">Animals</RouterLink>
+                <RouterLink class="link" to="/about">Community</RouterLink>
+                <RouterLink class="link" to="/about">Join</RouterLink>
+            </div>
+        </nav>
+    </header>
+    <RouterView />
+ </template>
+
+<style scoped>
+    .wrapper {
+        display:flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .link {
+        margin: 0.5em;
+        text-decoration: none;
+    }
+    nav {
+        background-color: #03045E;
+        height: 5em;
+    }
+    h1 {
+        color: white;
+    }
+    a {
+        font-size: medium;
+        color: white;
+    }
+</style>
