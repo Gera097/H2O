45e8a080b4cf9f4f9a6ef7aba894bfd4ac0cdba5
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-14 23:50:57 -0500

    Creando componente Button.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String
+  },
+});
+</script>
+
+<template>
+    <button>
+        {{msg}}
+    </button>
+</template>
+
+<style scoped>
+    button {
+        width:fit-content;
+        padding: 0.5em 1em;
+        background-color: #023E8A;
+        color: white;
+    }
+</style>
+
