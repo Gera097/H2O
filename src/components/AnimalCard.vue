0cdadb141ef10e734a36e1450920086766f0577e
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-15 11:38:06 -0500

    Creado componente AnimalCard.vue

--- src/components/AnimalCard.vue
+++ src/components/AnimalCard.vue
@@ -1,11 +1,40 @@
+<script setup>
+    defineProps(["name", "imgRoute"]);
+</script>
+
 <template>
-<div class="card">
-    <img src="@/assets/Ajolote.png" alt="Ajolote">
-    <button>{{animal_name}}</button>
-</div>
+<button class="card">
+    <img :src="imgRoute" alt="">
+    <p>{{name}}</p>
+</button>
 </template>
 
 <style scoped>
+    .card {
+        height: 400px;
+        width: 290px;
+        border: none;
+        background-color: #02023a00;
+    }
+
+    .card:hover p{
+        font-weight: 700;
+        color: white;
+        padding: 1em 2em;
+        background-color: #0A78B8;
+        border-radius: 0.5em;
+    }
+
+    p {
+        font-weight: 700;
+        color: white;
+        padding: 1em 2em;
+        background-color: #023E8A;
+        border-radius: 0.5em;
+    }
+
+    img {
+        border-radius: 1em;
+        width: 100%;
+    }
 </style>
-<script setup>
-</script>
