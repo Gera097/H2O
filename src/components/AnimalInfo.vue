9931c90a58aee63cd6aa019b9223a161fe65474d
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-15 16:36:11 -0500

    Creado componente AnimalInfo.vue

---
+++ src/components/AnimalInfo.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="card">
+        <div class="top">
+            <img :src="imgRoute" alt="">
+            <div class="animal-descript">
+                <h2>{{name}}</h2>
+                <p>{{description}}</p>
+            </div>
+        </div>
+        <div class="bottom">
+            <ul>
+                <li>Endangered Level: {{nivelPeligro}}</li>
+                <li>Region: {{region}}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps(["name", "imgRoute","description","nivelPeligro","region"]);
+</script>
+
+<style scoped>
+    .card {
+        margin: auto auto;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr 1fr;
+        grid-template-areas: 
+                            "top"
+                            "bottom";
+        color: #F6F6F6;
+        border-radius: 1em;
+        width: 70%;
+        max-height: 500px;
+        background-color: #023E8A;
+    }
+
+    img {
+        max-height: 150px;
+        border-radius: 1em;
+    }
+    .top {
+        grid-area: top;
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+        grid-template-rows: 1fr;
+        grid-template-areas: 
+                            "left right";
+        place-items: center;
+    }
+
+    .top img {
+        grid-area: left;
+    }
+
+    .top h2, .top p {
+        grid-area: right;
+        padding: 0em 15%;
+        text-align: justify;
+    }
+    .top h2 {
+        text-align: center;
+        text-decoration: underline;
+    }
+    .bottom {
+        text-align: center;
+        grid-area: bottom; 
+        margin: 5%;
+    }
+</style>
--- src/views/AnimalsView.vue
+++ src/views/AnimalsView.vue
@@ -1,11 +1,15 @@
 <script setup>
 import AnimalGrid from "@/components/AnimalGrid.vue";
 import AnimalFinder from "@/components/AnimalFinder.vue";
+import AnimalInfo from "@/components/AnimalInfo.vue";
+
 </script>
 <template>
     <div class="container">
-        <AnimalFinder class="buscador"/>
-        <AnimalGrid class="animal-grid"/>
+            <AnimalFinder class="buscador"/>
+            <AnimalInfo name="Mexican Ajolote" img-route="/src/assets/Ajolote.png" description="With the scientific name Ambystoma mexicanum, it is a creature whose curious appearance earned it the name of water monster (axolotl) among the Nahuatl tribe.
+It is possible to find it in the canals of the city of Xochimilco, but there are fewer and fewer specimens that exist in freedom, so most of them live in aquariums throughout the world." nivelPeligro="Extremo" region="Laguna de Xochimilco"/>
+            <AnimalGrid class="animal-grid"/>
     </div>
     
 </template>
