35ac32c2cce40acffb4369e6759f5c36112ab2a7
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-14 21:40:57 -0500

    Agregada imagen de portada Homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,13 @@ import TheWelcome from "@/components/TheWelcome.vue";
 
 <template>
   <main>
-    <TheWelcome />
+    <img src="@/assets/home_portrait.png" alt="Persona cortando basura atorada en su lancha, en el mar">
   </main>
 </template>
+
+<style scoped>
+  img {
+    width: 100%;
+    height: auto;
+  }
+</style>
