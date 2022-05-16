18973968bda04becf283d6b266fd0ad02a2a43c5
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-15 19:39:08 -0500

    deployment config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/H2O_client/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
