09d29b70571afdd264c3dd31e0b2ad5902a03d43
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-18 23:23:00 -0500

    Eliminado elemento AnimalInfo de AnimalsView

--- src/views/AnimalsView.vue
+++ src/views/AnimalsView.vue
@@ -1,14 +1,11 @@
 <script setup>
 import AnimalGrid from "@/components/AnimalGrid.vue";
 import AnimalFinder from "@/components/AnimalFinder.vue";
-import AnimalInfo from "@/components/AnimalInfo.vue";
 
 </script>
 <template>
     <div class="container">
             <AnimalFinder class="buscador"/>
-            <AnimalInfo name="Mexican Axolotl" img-route="https://www.24-horas.mx/wp-content/uploads/2019/11/ajolote.jpg?w=800" description="With the scientific name Ambystoma mexicanum, it is a creature whose curious appearance earned it the name of water monster (axolotl) among the Nahuatl tribe.
-            It is possible to find it in the canals of the city of Xochimilco, but there are fewer and fewer specimens that exist in freedom, so most of them live in aquariums throughout the world." nivelPeligro="Extreme" region="Laguna de Xochimilco"/>
             <AnimalGrid class="animal-grid"/>
     </div>
     
