fe9900fa96722c7ffcc046c6df9d363dcc16f83c
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-18 23:24:03 -0500

    Accionamiento de modal desde AnimalCard.vue

--- src/components/AnimalCard.vue
+++ src/components/AnimalCard.vue
@@ -1,21 +1,34 @@
 <script setup>
+    import {ref} from 'vue';
+    import AnimalInfoVue from "@/components/AnimalInfo.vue";
+    import ModalVue from '../components/Modal.vue';
     defineProps(["name", "imgRoute"]);
+    
+const showModal = ref(false)
 </script>
 
 <template>
-<button class="card">
+<Teleport to="body">
+    <ModalVue :show="showModal" @close="showModal = false">
+        <template #body>
+            <AnimalInfoVue name="Mexican Axolotl" img-route="https://www.24-horas.mx/wp-content/uploads/2019/11/ajolote.jpg?w=800" description="With the scientific name Ambystoma mexicanum, it is a creature whose curious appearance earned it the name of water monster (axolotl) among the Nahuatl tribe.
+        It is possible to find it in the canals of the city of Xochimilco, but there are fewer and fewer specimens that exist in freedom, so most of them live in aquariums throughout the world." nivelPeligro="Extreme" region="Laguna de Xochimilco"/>
+        </template>
+    </ModalVue>
+</Teleport>
+<button class="img-card" @click="showModal = true">
     <img :src="imgRoute" alt="">
     <p>{{name}}</p>
 </button>
 </template>
 
 <style scoped>
-    .card {
+    .img-card {
         border: none;
         background-color: #02023a00;
     }
 
-    .card:hover p{
+    .img-card:hover p{
         font-weight: 700;
         color: white;
         padding: 1em 2em;
