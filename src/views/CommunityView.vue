781b00d22a30dda56469ae8a6f9eb40d649cacef
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-05-20 02:08:17 -0500

    Prueba de ActiveEvents en página Community

--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -1,9 +1,15 @@
 <script setup>
-    
+    import ActiveEventCardVue from '../components/ActiveEventCard.vue';
 </script>
 <template>
-    <h2>Por el momento, la página de Comunidad se encuentra en desarrollo, pero pronto estará lista!! Vuelve pronto!</h2>
+    <ActiveEventCardVue img-route="./src/assets/fish_icon.svg" title="BEACH CLEANING" location="Mérida, Yucatán, México." date="6/6/22" n-days=2 n-hours=48 n-points=480000 class="active-event"/>
+    <ActiveEventCardVue img-route="./src/assets/water_bottle.svg" title="TRASH RECYCLING" location="Cancún, Quintana Roo, México." date="6/20/22" n-days=1 n-hours=8 n-points=80000 class="active-event"/>
+    
+
 </template>
 <style scoped>
-
+    .active-event {
+        margin: 1em 0px;
+        height: fit-content;
+    }
 </style>
