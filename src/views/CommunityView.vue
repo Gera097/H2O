ff8cf8633371d85c245a189cb4cb9d45e71f6134
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-06-06 17:59:22 -0500

    add user banner in community page

--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -1,9 +1,11 @@
 <script setup>
     import ActiveEventCardVue from '../components/ActiveEventCard.vue';
+    import UserBannerVue from '../components/UserBanner.vue';
 </script>
 <template>
     <ActiveEventCardVue img-route="./src/assets/fish_icon.svg" title="BEACH CLEANING" location="Mérida, Yucatán, México." date="6/6/22" n-days=2 n-hours=48 n-points=480000 class="active-event"/>
     <ActiveEventCardVue img-route="./src/assets/water_bottle.svg" title="TRASH RECYCLING" location="Cancún, Quintana Roo, México." date="6/20/22" n-days=1 n-hours=8 n-points=80000 class="active-event"/>
+    <UserBannerVue/>
     
 
 </template>
